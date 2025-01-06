0251ec3bc33ea8346421278e585c9631c741e5d3
NCAZO <nicolas.cazo@gustaveroussy.fr>
2025-01-06 18:26:44 +0100

    Initial

---
+++ src/main/java/com/openclassrooms/poc/PocMessagerieApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMessagerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMessagerieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/poc/PocMessagerieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMessagerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
